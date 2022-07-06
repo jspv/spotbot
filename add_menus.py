1b5035ec36e3c69026c7bdef78dd45959cdeb933
jspv <jspvgithub@twinleaf.xyz>
2022-07-05 22:22:55 -0400

    Moved menu configuraiton here for now

---
+++ add_menus.py
@@ -0,0 +1,57 @@
+import menu
+
+
+def add_menus(menu: menu.Menu) -> None:
+    menu.add_menu(
+        "main",
+        [
+            ("C", "Config Servo", "tbd"),
+            ("I", "Change increment µs", "menu.load_menu('us_increment')"),
+            ("N", "Change increment ∠", "menu.load_menu('angle_increment')"),
+            None,
+            ("L", "Load Config", "tbd"),
+            ("S", "Save Config", "tbd"),
+            None,
+            ("D", "Set Speed", "tbd"),
+            ("A", "Set Acceleration", "tbd"),
+            ("E", "Sequence Menu", "tbd"),
+            ("Q", "<-- Back", "menu.menu_backout"),
+        ],
+        title="[bold][u]Main Menu[/u][/bold]",
+    )
+    menu.add_menu(
+        "us_increment",
+        [
+            ("1", "1 µs", "set_us_increment(1)"),
+            ("2", "2 µs", "set_us_increment(2)"),
+            ("5", "5 µs", "set_us_increment(5)"),
+            ("x", "10 µs", "set_us_increment(10)"),
+            ("b", "20 µs", "set_us_increment(20)"),
+            ("l", "50 µs", "set_us_increment(50)"),
+            ("c", "100 µs", "set_us_increment(100)"),
+            ("d", "200 µs", "set_us_increment(200)"),
+            None,
+            ("Q", "<-- Back", "menu.menu_backout"),
+        ],
+        title="[bold][u]Servo Increment (µs)[u][/bold]",
+    )
+    menu.add_menu(
+        "angle_increment",
+        [
+            ("0", ".5°", "set_angle_increment(0.5)"),
+            ("1", "1°", "set_angle_increment(1.0)"),
+            ("2", "2°", "set_angle_increment(2.0)"),
+            ("5", "5°", "set_angle_increment(5.0)"),
+            ("x", "10°", "set_angle_increment(10.0)"),
+            (
+                "b",
+                "[bold][underline][italics][magenta]20°[/bold][/underline][/italics][/magenta]",
+                "set_angle_increment(20.0)",
+            ),
+            ("c", "45°", "set_angle_increment(45.0)"),
+            ("d", "90°", "set_angle_increment(90.0)"),
+            None,
+            ("Q", "<-- Back", "menu.menu_backout"),
+        ],
+        title="[bold][underline]Servo [magenta]Increment[/magenta] (∠)[/underline][/bold]",
+    )
